f3bf320ebfd7c48d82e3563c073f3d41717f8f3e
mihyeon <2_5_3_1@naver.com>
2021-04-09 13:24:44 +0900

    front

--- app/app.js
+++ app/app.js
@@ -5,9 +5,11 @@ const app = express();
  
 //앱 세팅
 const home = require("./src/routes/home");
-app.set("views",".src/views");
+app.set("views","./src/views");
 app.set("view engine" , "ejs");
  
 app.use("/", home);
-
+//src public 폴더 
+app.use(express.static(`${__dirname}/src/public`));
 module.exports = app;
+
